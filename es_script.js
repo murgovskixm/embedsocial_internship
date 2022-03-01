51fdb2b21798ecbd4ff28f46df4fa17392006ca1
Martin Murgovski <murgovskixm@gmail.com>
2022-03-01 16:11:01 +0100

    done

--- es_script.js
+++ es_script.js
@@ -13,7 +13,8 @@ function createCard(index) {
     element.appendChild(profilePic);
     let name = document.createElement('div');
     name.className = 'name';
-    name.innerHTML = "<p style = 'font-weight: bold; font-family: Bahnschrift,serif'>" + jsonData[index].name + "</p>" + jsonData[index].date;
+    name.innerHTML = "<p style = 'font-weight: bold; font-family: Bahnschrift,serif'>"
+        + jsonData[index].name + "</p>" + jsonData[index].date.split(" ")[0];
     element.appendChild(name);
     let insta = document.createElement('div');
     insta.className = 'insta';
@@ -25,17 +26,29 @@ function createCard(index) {
     element.appendChild(pic);
     let desc = document.createElement('div');
     desc.className = 'desc';
-    desc.innerHTML = "<p style = 'font-family: Calibri,serif; text-overflow: ellipsis;'>" + jsonData[index].caption + "</p>";
+    desc.innerHTML = "<p style = 'font-family: Calibri,serif; text-overflow: ellipsis;'>"
+        + jsonData[index].caption + "</p>";
     element.appendChild(desc);
     let heart = document.createElement('div');
     heart.className = 'heart';
-    heart.innerHTML = "<img src='icons/heart.svg'>";
+    heart.innerHTML = "<input class='like' type='image' src='icons/heart.svg' onclick='like(" + index + ")'>";
     element.appendChild(heart);
     let likes = document.createElement('div');
     likes.className = 'likes';
     likes.innerHTML = jsonData[index].likes;
     element.appendChild(likes);
     return element;
+}
+
+function like(index) {
+    if (document.getElementsByClassName('like')[index].style.backgroundColor === "red") {
+        document.getElementsByClassName('like')[index].style.backgroundColor = "transparent";
+        document.getElementsByClassName('likes')[index].innerHTML = jsonData[index].likes;
+    }
+    else {
+        document.getElementsByClassName('like')[index].style.backgroundColor = "red";
+        document.getElementsByClassName('likes')[index].innerHTML = parseInt(jsonData[index].likes) + 1;
+    }
 }
 
 function loadItems() {
@@ -49,6 +62,8 @@ function loadItems() {
 
 function loadMore() {
     loadItems();
+    let element = document.getElementById('load');
+    element.scrollIntoView({behavior: "smooth", inline: "end"});
     if (currIndex === jsonData.length) {
         document.getElementById("load").remove();
     }
